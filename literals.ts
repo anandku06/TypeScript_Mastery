287f66ba6df77abf88c7a9835094b71f7c57c87d
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-12 18:45:38 +0530

    add examples of literal types in literals.ts

---
+++ literals.ts
@@ -0,0 +1,11 @@
+let weekends: "Saturday" | "Sunday"
+// now this variable can take only these two values, not other than that
+
+// weekends = "Friday"; // throws error
+
+let rgb: "red" | "green" | "blue";
+// rgb = "black" // throws an error
+
+let bool: true | false
+
+// bool = 23 // throws an error
