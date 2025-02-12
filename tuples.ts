633603583594325d72fedb3a098adcb7b8c7c679
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-12 19:08:06 +0530

    add tuple examples in tuples.ts with comments on sequence validation

---
+++ tuples.ts
@@ -0,0 +1,10 @@
+let myTuple: [number, string]
+// tuple is defined and consider the sequence as well
+myTuple = [123, "hello"] // giving the value according to the sequence given
+// myTuple = ["hello", 123] // if sequence is reversed then it throws an error
+
+console.log(myTuple)
+
+const games: [string, string, string] = ["NFS", "GOW", "GOT"]
+
+// games = [123, "GOWR", "GTA-V"] // throws an error as the tuple condition not met
