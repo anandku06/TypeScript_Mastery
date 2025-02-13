5939a48aa5382cdcb4909c37f1b6d34a0e22ce5e
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-13 19:06:25 +0530

    add Person class with constructor and example instantiation

---
+++ classes.ts
@@ -0,0 +1,17 @@
+// here a class Person is made and the parameters used are also declared with the annotations
+export class Person{
+    // now these properties can't be modified
+    readonly name : string 
+    readonly age : number 
+
+    constructor(name: string, age: number){
+        this.name = name
+        this.age = age
+    }
+}
+
+// making an object of the class
+const man = new Person("Anand", 20)
+console.log(man);
+
+man.age = 24 // gives an error; can't be modified
