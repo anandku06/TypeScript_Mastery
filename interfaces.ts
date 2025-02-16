9b0d4056888dfbcc35e22ffa6b6657e050ebf4dd
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-16 15:23:58 +0530

    add Movie interface with readonly and optional properties in interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -13,3 +13,25 @@ const compEx: Computer = {
 }
 
 console.log(compEx)
+
+interface Movie{
+    readonly name : string
+    ratings : number
+    genre? : string
+}
+
+const movie1 : Movie = {
+    name : "Mission Impossible 4",
+    ratings : 9.1,
+    genre : "action"
+}
+
+// movie1.name = "John Wick 3" // can't change bcz of readonly property
+
+const movie2 : Movie = {
+    name : "OverFlow",
+    ratings : 8.9
+} // due to optional property genre can be neglected
+
+console.log(movie1)
+console.log(movie2)
