fab308aa01cf77d9fc9c59bcd50ef8e078ade83d
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-16 16:05:10 +0530

    refactor interfaces.ts to enhance Computer and Movie interfaces, add MathOperation interface, and implement Person interface with greet function

--- interfaces.ts
+++ interfaces.ts
@@ -1,37 +1,75 @@
 // interface for any variable
-interface Computer{
-    name : string
-    RAM : number
-    HDD : number
+interface Computer {
+  name: string;
+  RAM: number;
+  HDD: number;
 }
 
 // this variable implements this interface and now it should have the three parameters the Computer has
 const compEx: Computer = {
-    name : "i9",
-    RAM : 16,
-    HDD : 100
+  name: "i9",
+  RAM: 16,
+  HDD: 100,
+};
+
+console.log(compEx);
+
+interface Movie {
+  readonly name: string;
+  ratings: number;
+  genre?: string;
 }
 
-console.log(compEx)
-
-interface Movie{
-    readonly name : string
-    ratings : number
-    genre? : string
-}
-
-const movie1 : Movie = {
-    name : "Mission Impossible 4",
-    ratings : 9.1,
-    genre : "action"
-}
+const movie1: Movie = {
+  name: "Mission Impossible 4",
+  ratings: 9.1,
+  genre: "action",
+};
 
 // movie1.name = "John Wick 3" // can't change bcz of readonly property
 
-const movie2 : Movie = {
-    name : "OverFlow",
-    ratings : 8.9
-} // due to optional property genre can be neglected
+const movie2: Movie = {
+  name: "OverFlow",
+  ratings: 8.9,
+}; // due to optional property genre can be neglected
 
-console.log(movie1)
-console.log(movie2)
+console.log(movie1);
+console.log(movie2);
+
+// interfaces regarding functions
+interface MathOperation {
+  (x: number, y: number): number;
+}
+
+const add: MathOperation = (a, b) => a + b;
+const subtract: MathOperation = (a, b) => a - b;
+
+// console.log(add("12", "34")) // if given params other than number then error is thrown
+console.log(add(12, 34)); 
+console.log(subtract(12, 34)); 
+
+// interfaces for methods
+export interface Person{
+    fName : string
+    lName : string
+    age : number
+    sayHello() : void
+}
+
+function greet(person : Person){
+    console.log(`Hello, ${person.fName} ${person.lName}`)
+    person.sayHello()
+}
+
+const John : Person = {
+    fName : "John",
+    lName : "Wick",
+    age : 30,
+    sayHello() {
+        console.log("Hi there!!")
+    },
+}
+
+greet(John)
+console.log(John)
+John.sayHello()
