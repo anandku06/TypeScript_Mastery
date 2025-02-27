eb31e0a0aac4f83282178476973dd42c48cb9dd9
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-27 11:48:45 +0530

    add new JavaScript files for arrays, enums, getters/setters, index, and interfaces; update tsconfig.json to include these files

---
+++ arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+const nums = [1, 2, 3, 4, 5]; // array of type number
+const strNums = ["one", "two", "three"]; // array of type string
+// nums.push("six") // throws an error bcz of type mismatch
+console.log(nums);
+console.log(strNums);
+const foods = ["pizza", "burger", "pasta"]; // another type of initialisation
+console.log(foods);
