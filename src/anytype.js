59679cf7c20ba516de4c7a8f485415c460ea721d
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-27 11:59:07 +0530

    remove unused JavaScript files for arrays, classes, enums, generics, getters/setters, index, interfaces, access modifiers, any type, function parameters, and inferring

--- src/anytype.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let color = "crimson";
-console.log(color);
-color = true;
-color = 123;
-color = {
-    name: "red"
-};
-// here the color var can take any data despite of its type, TS not at all type checking the data
--- src/inferring.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let tech = "TypeScript"; // we don't specified the annotation here
-// TS took the datatype by seeing the value of that variable
-// tech = 123 // throws error as reassigning with changed datatype
-let nu = 123; // here as well TS assigned the datatype without annotation
-console.log(nu);
-console.log(tech);
-console.log(typeof tech);
